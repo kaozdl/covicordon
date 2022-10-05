8171225709de4004bd8855786a3efb0b15a678e2
Kalil de Lima <kaozdl@gmail.com>
2022-10-05 20:45:35 -0300

    add dj-database-url

--- coovicordon/settings.py
+++ coovicordon/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -96,10 +98,8 @@ DATABASES = {
 PRODUCTION = os.environ.get('PRODUCTION', False)
 if PRODUCTION:
     DATABASE_URL = os.environ.get('DATABASE_URL')
-    DATABASES = {
-        'default': DATABASE_URL
-    }
-
+    DATABASES['default'] = dj_database_url.config(DATABASE_URL)
+    DATABASES['default']['ATOMIC_REQUESTS'] = True
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
