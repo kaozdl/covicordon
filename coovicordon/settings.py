9c9c5a49b6edeed2a27704be77fbfbb5592d20f4
Kalil de Lima <kaozdl@gmail.com>
2022-10-05 20:30:25 -0300

    add database url

--- coovicordon/settings.py
+++ coovicordon/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -93,6 +93,12 @@ DATABASES = {
     }
 }
 
+PRODUCTION = os.environ.get('PRODUCTION', False)
+if PRODUCTION:
+    DATABASE_URL = os.environ.get('DATABASE_URL')
+    DATABASES = {
+        'default': DATABASE_URL
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
